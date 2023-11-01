b1871c9bee0e5fe84dfcf8d7a6244064f86296cb
Faramarz Aali <fara.e1993@yahoo.com>
2023-11-01 03:45:31 +0330

    adding touch feature for hamburger  menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const navBtn = document.querySelector(".nav__toggle-icon")
 const menu = document.querySelector(".menu")
 const cover = document.querySelector(".cover")
+const body = document.querySelector("body")
 
 navBtn.addEventListener("click",function(){
     navBtn.classList.toggle("nav__toggle-icon--open")
@@ -12,4 +13,11 @@ navBtn.addEventListener("click",function(){
     /*یا*/
     menu.classList.toggle("menu--open")
     cover.classList.toggle("cover--show")
+})
+
+body.addEventListener("touch",function(){
+    if(menu.classList.contains("menu--open")){
+        menu.classList.remove("menu--open")
+        navBtn.classList.remove("nav__toggle-icon--open")
+    }
 })
