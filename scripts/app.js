8415a47709dad618616fe9b32f3140ae8c262a6a
Faramarz Aali <fara.e1993@yahoo.com>
2023-11-01 04:08:07 +0330

    fixed adding touch feature for hamburger menu

--- scripts/app.js
+++ scripts/app.js
@@ -23,7 +23,7 @@ navBtn.addEventListener("click",function(){
     console.log(hamburger_checker)
 })
 
-menu.addEventListener("touch",function(){
+menu.addEventListener("touchstart",function(){
    if(hamburger_checker){
         menu.classList.remove("menu--open") 
         navBtn.classList.remove("nav__toggle-icon--open")  
