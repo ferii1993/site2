b4bfa3c5e048c9c060c8af745b40ccd5d227a777
Faramarz Aali <fara.e1993@yahoo.com>
2023-11-01 04:03:03 +0330

    fixed adding touch feature for hamburger menu bug

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const menu = document.querySelector(".menu")
 const cover = document.querySelector(".cover")
 const body = document.querySelector("body")
 
+let hamburger_checker = false
+
 navBtn.addEventListener("click",function(){
     navBtn.classList.toggle("nav__toggle-icon--open")
     /*if(navBtn.classList.contains("nav__toggle-icon--open")){
@@ -13,11 +15,18 @@ navBtn.addEventListener("click",function(){
     /*یا*/
     menu.classList.toggle("menu--open")
     cover.classList.toggle("cover--show")
+    if(menu.classList.contains("menu--open")){
+        hamburger_checker = true
+    }else{
+        hamburger_checker = false
+    }
+    console.log(hamburger_checker)
 })
 
-body.addEventListener("touch",function(){
-    if(menu.classList.contains("menu--open")){
-        menu.classList.remove("menu--open")
-        navBtn.classList.remove("nav__toggle-icon--open")
-    }
+menu.addEventListener("touch",function(){
+   if(hamburger_checker){
+        menu.classList.remove("menu--open") 
+        navBtn.classList.remove("nav__toggle-icon--open")  
+        cover.classList.remove("cover--show") 
+   }
 })
